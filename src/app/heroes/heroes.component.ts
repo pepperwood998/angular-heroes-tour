5c192e85b7c43c2841f6df331f68dfc18a579240
TUAN Dao Thien (VTI.D2) <tuan.daothien@vti.vn>
2020-08-10 16:27:30 +0700

    Init first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  // life-cycle hook
+  // initialization logic
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    // subscribe to asyc method and add callback
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+}
